84a58c18f4f25ce876a4da0e6340142c27edcb70
Hanad <45791558+ksosos@users.noreply.github.com>
2022-04-24 15:31:30 +0200

    Update local.lua

--- local.lua
+++ local.lua
@@ -1,26 +1,21 @@
 conn = script.Parent.MouseButton1Click:Connect(function()
-	print("Xx")
-	local Player = game.Players.LocalPlayer
-	local character = Player.Character or Player.CharacterAdded:Wait()
-	local Humanoid = character:WaitForChild("Humanoid")
+        print("Xx")
+        local Player = game.Players.LocalPlayer
+        local character = Player.Character or Player.CharacterAdded:Wait()
+        local Humanoid = character:WaitForChild("Humanoid")
 
-	for _,item in pairs(Player.Character:GetDescendants()) do
-		if item.Name:lower() == "guitar" then
-			local guitar = item
-			local Music = guitar.Handle.GuitarSong
-					game.ReplicatedStorage:WaitForChild("ueue"):FireServer(Music)
-					--Music:Play()
-					print"recieved"										
-					print("Anim and song played!")
-				
-
-
-			
-		end
-	end
-end )
+        for _, item in pairs(Player.Character:GetDescendants()) do
+            if item.Name:lower() == "guitar" then
+                local guitar = item
+                local Music = guitar.Handle.GuitarSong
+                game.ReplicatedStorage:WaitForChild("ueue"):FireServer(Music)
+                print "recieved"
+            end
+        end
+    end)
 pcall(function()
-	script.Parent.Parent.Parent.Parent.Parent.Unequipped:Connect(function()
-		conn:Disconnect()
-	end)
+        script.Parent.Parent.Parent.Parent.Parent.Unequipped:Connect(
+            function()
+                conn:Disconnect()
+            end)
 end)
